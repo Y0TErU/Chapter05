ad60b19c020fd67101fa36cee47d4fcfd0cc6193
warusurayooteru <warusura.yooteru@gmail.com>
2024-10-15 14:20:19 +0900

    04-04 04-01~03は必要ないと判断したので割愛

--- Chapter05/Main.cpp
+++ Chapter05/Main.cpp
@@ -1,29 +1,35 @@
 #include<iostream>
 using namespace std;
 
+const int MAX_NAME = 16;
+struct Student 
+{
+	char name[MAX_NAME + 1];
+	int score_japanese;
+	int score_math;
+	int score_English;
+};
+
+void ShowScore(const Student& student)
+{
+	cout << "–¼‘O: " << student.name << endl
+		<< "‘Œê: " << student.score_japanese << flush
+		<< "”Šw: " << student.score_math << flush
+		<< "‰pŒê: " << student.score_English << endl;
+}
+
 int main()
 {
-	const int KUKU_SIZE = 9;
-	int kuku[KUKU_SIZE][KUKU_SIZE];
+	Student student[] =
+	{ 
+	{"“ú“o—z‹P",73,98,86,},
+	{"“ì•”—Dl",64,45,40,},
+	{"‹g“cŠ",76,78,69},
+	};
+	int size = sizeof student / sizeof * student;
 
-	for (int i = 1; i <= KUKU_SIZE; i++)
+	for (int i = 0; i < size; i++)
 	{
-		for (int j = 1; j <= KUKU_SIZE; j++)
-		{
-			kuku[i - 1][j - 1] = i * j;
-		}
+		ShowScore(student[i]);
 	}
-	printf("   1  2  3  4  5  6  7  8  9\n");
-
-	for (int i = 1; i <= KUKU_SIZE; i++)
-	{
-		printf("%d ", i);
-		for (int j = 1; j <= KUKU_SIZE; j++)
-		{
-			printf("%2d ", kuku[i - 1][j - 1]);
-		}
-		cout << endl;
-	}
-
-
 }
