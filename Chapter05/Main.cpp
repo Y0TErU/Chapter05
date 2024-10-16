e1e508c90180622ed44bc4f79692d9ffa146ed46
warusurayooteru <warusura.yooteru@gmail.com>
2024-10-16 15:32:42 +0900

    05-07

--- Chapter05/Main.cpp
+++ Chapter05/Main.cpp
@@ -1,35 +1,15 @@
 #include<iostream>
 using namespace std;
 
-const int MAX_NAME = 16;
-struct Student 
-{
-	char name[MAX_NAME + 1];
-	int score_japanese;
-	int score_math;
-	int score_English;
-};
-
-void ShowScore(const Student* pointer)
-{
-	cout << "–¼‘O: " << pointer->name << endl
-		<< "‘Œê: " << pointer->score_japanese << flush
-		<< "”Šw: " << pointer->score_math << flush
-		<< "‰pŒê: " << pointer->score_English << endl;
-}
-
 int main()
 {
-	Student student[] =
-	{ 
-	{"“ú“o—z‹P",73,98,86,},
-	{"“ì•”—Dl",64,45,40,},
-	{"‹g“cŠ",76,78,69},
-	};
-	int size = sizeof student / sizeof * student;
+	int a, b;
 
-	for (int i = 0; i < size; i++)
-	{
-		ShowScore(&student[i]);
-	}
+	cout << "2‚Â‚Ì’l‚ð“ü—Í‚µ‚Ä‰º‚³‚¢ > " << flush;
+	cin >> a >> b;
+
+	cout << "‘å‚«‚¢•û‚Ì’l‚Í " << (a > b ? a : b) << " ‚Å‚·B" << endl;
+
 }
+
+
